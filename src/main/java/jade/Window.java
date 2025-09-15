5e4bbec4a8cbaa69b29cd3e25ecf9bf9fcffe0ca
EmpyrianForge <kvnbaunach@googlemail.com>
2025-09-15 20:14:30 +0200

    Main

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -4,6 +4,7 @@ import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
 
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11C.*;
 import static org.lwjgl.system.MemoryUtil.NULL;
@@ -34,6 +35,14 @@ public class Window {
 
         init();
         loop();
+
+        //Free the Memory
+        glfwFreeCallbacks(glfwWindow);
+        glfwDestroyWindow(glfwWindow);
+
+        //Terminate GLFW and the free the error callback
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
     }
         public void init() {
             // Setup an error callback
@@ -73,7 +82,7 @@ public class Window {
                 //Poll events
                 glfwPollEvents();
 
-                glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+                glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
                 glClear(GL_COLOR_BUFFER_BIT);
 
                 glfwSwapBuffers(glfwWindow);
