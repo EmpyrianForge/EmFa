a7d5105625150ec7f75214a781e0c359580542a8
EmpyrianForge <kvnbaunach@googlemail.com>
2025-10-05 11:05:51 +0200

    Main

--- src/main/java/renderer/RenderBatch.java
+++ src/main/java/renderer/RenderBatch.java
@@ -1,10 +1,11 @@
 package renderer;
 
 import components.SpriteRenderer;
+import jade.Window;
+import org.joml.Vector4f;
 
 import static org.lwjgl.opengl.GL15.*;
-import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
-import static org.lwjgl.opengl.GL20.glVertexAttribPointer;
+import static org.lwjgl.opengl.GL20.*;
 import static org.lwjgl.opengl.GL30.glBindVertexArray;
 import static org.lwjgl.opengl.GL30.glGenVertexArrays;
 
@@ -69,12 +70,87 @@ public class RenderBatch {
         glEnableVertexAttribArray(1);
     }
 
+    public void addSprite(SpriteRenderer spr) {
+        // Get index and add RederObject
+        int index = this.numSprites;
+        // [0, 1, 2, 3, 4, 5]
+        this.sprites[index] = spr;
+        this.numSprites++;
+
+        //Add properties to local vertices array
+        loadVertexProperties(index);
+
+        if (numSprites >= this.maxBatchSize) {
+            this.hasRoom = false;
+        }
+    }
+
+    public void render() {
+        // For no we will rebuffer all data every frame
+        glBindBuffer(GL_ARRAY_BUFFER, vboID);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, vertices);
+
+        //Use shader
+        shader.use();
+        shader.uploadMat4f("uProjection", Window.getScene().camera().getProjectionMatrix());
+        shader.uploadMat4f("uView", Window.getScene().camera().getViewMatrix());
+
+        glBindVertexArray(vaoID);
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
+
+        glDrawElements(GL_TRIANGLES, this.numSprites * 6, GL_UNSIGNED_INT, 0);
+
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+        glBindVertexArray(0);
+
+        shader.detach();
+    }
+
+    private void loadVertexProperties(int index) {
+        SpriteRenderer sprite = this.sprites[index];
+
+        //find offset within array (4 vertices per sprite)
+        int offset = index * 4 * VERTEX_SIZE;
+        // FLoat Float           Float FLoat FLoatt FLoat  Float
+
+        Vector4f color = sprite.getColor();
+
+        // Add vertices with appropriate properties
+        float xAdd = 1.0f;
+        float yAdd = 1.0f;
+        for (int i=0; i < 4; i++) {
+            if (i == 1) {
+                yAdd = 0.0f;
+            } else if (i ==2) {
+                xAdd = 0.0f;
+            } else if (i == 3) {
+                yAdd = 1.0f;
+            }
+
+            // Load position
+            vertices[offset] = sprite.gameObject.transform.position.x + (xAdd * sprite.gameObject.transform.scale.x);
+            vertices[offset + 1] = sprite.gameObject.transform.position.y + (yAdd * sprite.gameObject.transform.scale.y);
+
+            // Load color
+            vertices[offset + 2] = color.x;
+            vertices[offset + 3] = color.y;
+            vertices[offset + 4] = color.z;
+            vertices[offset + 5] = color.w;
+
+            offset *= VERTEX_SIZE;
+        }
+    }
+
     private int[] generateIndices() {
         // 6 indices per quad ( 3 per triangle)
         int[] elements = new int[ 6 * maxBatchSize];
         for (int i=0; i < maxBatchSize; i++) {
             loadElementIndices(elements, i);
         }
+
+        return elements;
     }
 
     private void loadElementIndices(int[] elements, int index) {
@@ -86,6 +162,11 @@ public class RenderBatch {
         elements[offsetArrayIndex] = offset + 3;
         elements[offsetArrayIndex +1] = offset + 2;
         elements[offsetArrayIndex + 2] = offset + 0;
+
+        // Triangle 2
+        elements[offsetArrayIndex +1] = offset + 0;
+        elements[offsetArrayIndex + 2] = offset + 2;
+        elements[offsetArrayIndex + 2] = offset + 1;
 
     }
 }
